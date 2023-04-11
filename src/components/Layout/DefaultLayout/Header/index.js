b60ef3e9d8beeb2f4c5689ed9ddcb74ba76cbdb4
NguyenDO <zmann22102002@gmail.com>
2023-04-11 13:03:13 +0700

    Xay dung UI phan header #2

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,27 +1,50 @@
+import { useEffect, useState } from 'react'
 import styles from './Header.module.scss'
 import classNames from 'classnames/bind'
 import images from '../../../../assets/images'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faCircleXmark, faSpinner, faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Tippy from '@tippyjs/react/headless'
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { Wrapper as PopperWrapper } from '../../../../components/Popper'
 const cx = classNames.bind(styles)
 function Header() {
+    const [searchResult, setSearchResult] = useState([])
+    useEffect(() => {
+        setTimeout(() => {
+            setSearchResult([1, 2, 3])
+        }, 3000)
+    }, [])
     return (
         <header className={cx('wrapper')} >
             <div className={cx('inner')}>
                 <div className={cx('logo')}>
-                    <img src = {images.logo} alt='Loi LoGo'/>
+                    <img src={images.logo} alt='Loi LoGo' />
                 </div>
-                <div className={cx('search')}>
-                    <input placeholder='search account or videos' spellCheck = {false}>
-                    </input>
-                    <button className={cx('clear')}>
-                        <FontAwesomeIcon icon={faCircleXmark}/>
-                    </button>
-                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner}/>
-                    <button className = {cx('search-btn')}>
-                    <FontAwesomeIcon icon={faMagnifyingGlass}/>
-                    </button>
-                </div>
+
+                <Tippy
+                    visible={searchResult.length > 0}
+                    render={attrs => {
+                        return (
+                            <div className={cx('search-result')} tabIndex={-1}>
+                            <PopperWrapper>
+                                Kết Quả
+                            </PopperWrapper>
+                            </div>
+                        )
+                    }}
+                >
+                    <div className={cx('search')}>
+                        <input placeholder='search account or videos' spellCheck={false}>
+                        </input>
+                        <button className={cx('clear')}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                        <button className={cx('search-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                    </div>
+                </Tippy>
                 <div className={cx('actions')}></div>
             </div>
         </header>
